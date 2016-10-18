589ffdaac50f37d9e136dced10a1db4e1702ecea
CoderAFI <developer_afi@163.com>
2016-10-18 18:44:26 +0800

    update mdes.

--- Scripts/tools.sh
+++ Scripts/tools.sh
@@ -24,4 +24,5 @@ make install #将安装到/usr/local/bin下
 # tmux
 brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+brew install reattach-to-user-namespace
 # copy the config file and press `prefix + I` install plugins
