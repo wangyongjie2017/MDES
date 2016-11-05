78aa35facbd2db0b2c12d463afa95c88d7d4398f
CoderAFI <developer_afi@163.com>
2016-11-05 13:04:09 +0800

    update sitesucker.

--- Scripts/cask.sh
+++ Scripts/cask.sh
@@ -14,8 +14,8 @@ cp ~/ROOT/codes/github/MDES/Sync/atom-config.cson ~/.atom/config.cson
 # visual studio code
 brew cask install visual-studio-code
 
-# Chrome WWDC Dropbox cheatsheet sourcetree transmit foxmail telegram slack
-brew cask install google-chrome wwdc dropbox cheatsheet sourcetree foxmail telegram slack insomnia
+# Chrome WWDC Dropbox cheatsheet sourcetree transmit foxmail telegram slack insomnia sitesucker
+brew cask install google-chrome wwdc dropbox cheatsheet sourcetree foxmail telegram slack insomnia sitesucker
 
 # Intellij
 brew cask install intellij-idea webstorm pycharm android-studio
