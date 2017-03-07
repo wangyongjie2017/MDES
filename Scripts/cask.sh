d55ff062378e6ba0bd7b770edc0eb8e6516ac055
CoderAFI <developer_afi@163.com>
2017-03-07 23:11:35 +0800

    add language rust.

--- Scripts/cask.sh
+++ Scripts/cask.sh
@@ -6,7 +6,7 @@ curl ip.gs
 
 # Atom
 brew cask install atom
-apm install file-icons git-time-machine pigments project-manager emmet atom-beautify language-swift markdown-writer
+apm install file-icons git-time-machine pigments project-manager emmet atom-beautify language-swift markdown-writer language-rust
 # 可选安装 apm install wakatime nuclide omnisharp autocomplete-modules atom-ternjs atom-css-comb sync-settings
 apm install nucleus-dark-ui Seti-ui seti-syntax
 cp ~/ROOT/codes/github/MDES/Sync/atom-config.cson ~/.atom/config.cson
