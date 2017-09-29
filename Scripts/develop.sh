f654cdfb825b94e28c5cd347bac73eee7ffa818c
CoderAFI <developer_afi@163.com>
2017-09-29 10:21:15 +0800

    add rubymine.

--- Scripts/develop.sh
+++ Scripts/develop.sh
@@ -117,7 +117,7 @@ code --list-extensions
 brew cask install google-chrome wwdc dropbox cheatsheet sourcetree telegram slack insomnia sitesucker hyper spark
 
 # Intellij
-brew cask install intellij-idea webstorm pycharm android-studio appcode #(android sdk)
+brew cask install intellij-idea webstorm pycharm rubymine android-studio appcode #(android sdk)
 
 # genymotion
 brew cask install virtualbox genymotion
